ce14383a366eda8756ab04283b7fe5f1dcf913f7
mehak aijaz <mehakaijaz716@gmail.com>
2024-05-05 15:38:41 +0530

    final commit

---
+++ base/tests/test_forms.py
@@ -0,0 +1,21 @@
+
+from django.test import SimpleTestCase
+from base.forms import ExpenseForm
+
+
+class TestForms(SimpleTestCase):
+    #form with valid data
+    def test_expense_forms_valid_data(self):
+        form = ExpenseForm(data={
+            'title':'expense1',
+            'amount':1000,
+            'category':'development'
+        })
+
+        self.assertTrue(form.is_valid(), "Test Failed")
+
+    #form without data
+    def test_expense_forms_no_data(self):
+        form = ExpenseForm(data={})
+        self.assertFalse(form.is_valid(), "Test Failed")
+        self.assertEquals(len(form.errors), 3, "Test Failed")
--- budget_project/settings.py
+++ budget_project/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'budget_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'budget',
+        'USER':"postgres",
+        "PASSWORD":"azmi1234",
+        "HOST":"localhost",
+        "PORT":"5432",
     }
 }
 
