9bfb0e1fa9932e505695c2a85976d88a35be002f
mehak aijaz <mehakaijaz716@gmail.com>
2024-05-05 14:49:48 +0530

    third commit

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Project(models.Model):
+    name=models.CharField(max_length=100)
+    slug=models.SlugField(max_length=100,unique=True,blank=True)
+    budget=models.IntegerField()
+    
+    def save(self,*args, **kwargs):
+        self.slug=slugify(self.name)
+        super(Project,self).save(*args, **kwargs)
+        
+    @property
+    def budget_left(self):
+        pass
+    
+    @property
+    def total(self):
+        pass
+    
+
+    
