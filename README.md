14805010d94c86fc220e2edb79347d843b5f1fb5
mehak aijaz <mehakaijaz716@gmail.com>
2024-05-22 00:00:16 +0530

    tests_urls edited

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #django testing
+![alt text]https://github.com/mehakaijaz/django_testing/blob/main/budget_project.PNG
--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,5 @@ from .views import home, ProjectCreateView, project_detail
 urlpatterns = [
    path('', home, name='home'),
    path('add/', ProjectCreateView.as_view(), name='add'),
-   path('<slug:project_slug>/', project_detail, name='detail')
+   path('<slug:project_slug>/', project_detail, name='detail'),
 ]
